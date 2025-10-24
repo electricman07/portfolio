2ba2b7051d0dce2789e2698120902ee1f77a30b7
Glen Popowich <glenp@telus.net>
2025-10-24 14:50:05 -0700

    setup page layout, theme and language toggles

--- src/components/AppWrapper/AppWrapper.tsx
+++ src/components/AppWrapper/AppWrapper.tsx
@@ -1,11 +1,58 @@
 "use client";
 
-import { THEME } from "@/utils/theme";
-import React from "react";
+import { THEME, ITheme } from "@/utils/theme";
+import React, { createContext, useState } from "react";
 import { ThemeProvider } from "styled-components";
+import {
+  AppContainer,
+  ContentContainer,
+  GlobalStyle,
+} from "./AppWrapper.styles";
+import AboutMe from "./AboutMe/AboutMe";
+import { ILanguageTexts, LANGUAGE } from "@/utils/text";
+
+interface IAppContext {
+  theme: ITheme;
+  toggleTheme: () => void;
+  currentLanguage: "en" | "fr";
+  toggleLanguage: () => void;
+  languageTexts: ILanguageTexts;
+  themeType: "light" | "dark";
+}
+
+export const AppContext = createContext<IAppContext | null>(null);
 
 const AppWrapper = ({ children }: { children: React.ReactNode }) => {
-  return <ThemeProvider theme={THEME.light}>{children}</ThemeProvider>;
+  const [theme, setTheme] = useState<"light" | "dark">("dark");
+  const [language, setLanguage] = useState<"en" | "fr">("en");
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  const toggleLanguage = () => {
+    setLanguage(language === "fr" ? "en" : "fr");
+  };
+
+  const appContextValue = {
+    theme: THEME[theme],
+    themeType: theme,
+    languageTexts: LANGUAGE[language],
+    toggleTheme,
+    toggleLanguage,
+    currentLanguage: language,
+  };
+  return (
+    <AppContext.Provider value={appContextValue}>
+      <ThemeProvider theme={THEME[theme]}>
+        <GlobalStyle />
+        <AppContainer>
+          <AboutMe />
+          <ContentContainer>{children}</ContentContainer>
+        </AppContainer>
+      </ThemeProvider>
+    </AppContext.Provider>
+  );
 };
 
 export default AppWrapper;
