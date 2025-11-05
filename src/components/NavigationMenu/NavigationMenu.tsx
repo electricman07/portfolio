66364f236a194620ebf06584e2032930728097a7
Glen Popowich <glenp@telus.net>
2025-11-05 13:29:39 -0800

    added theme icons

--- src/components/NavigationMenu/NavigationMenu.tsx
+++ src/components/NavigationMenu/NavigationMenu.tsx
@@ -6,18 +6,33 @@ import {
 import { AppContext } from "../AppWrapper/AppWrapper";
 import { RxFontFamily } from "react-icons/rx";
 import { RiEnglishInput } from "react-icons/ri";
+import { TbBulb, TbBulbOff } from "react-icons/tb";
 
 const NavigationMenu = () => {
-  const { currentLanguage, theme } = useContext(AppContext);
+  const { currentLanguage, theme, toggleLanguage, themeType, toggleTheme } =
+    useContext(AppContext);
   const languageIcon =
     currentLanguage === "en" ? (
       <RxFontFamily color={theme.textColor} size={"1.2vw"} />
     ) : (
       <RiEnglishInput color={theme.textColor} size={"1.2vw"} />
     );
+  const themeIcon =
+    themeType === "light" ? (
+      <TbBulb color={theme.textColor} size={"1.2vw"} />
+    ) : (
+      <TbBulbOff color={theme.textColor} size={"1.2vw"} />
+    );
   return (
     <NavigationMenuContainer>
-      <NavigationMenuAction>{languageIcon}</NavigationMenuAction>
+      <NavigationMenuAction onClick={() => toggleLanguage()}>
+        {languageIcon}
+        <p className="tooltip">Change Language</p>
+      </NavigationMenuAction>
+      <NavigationMenuAction onClick={() => toggleTheme()}>
+        {themeIcon}
+        <p className="tooltip">Change Theme</p>
+      </NavigationMenuAction>
     </NavigationMenuContainer>
   );
 };
