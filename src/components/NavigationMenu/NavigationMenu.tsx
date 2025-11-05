49fd3611420d1cf8ada0d623ce9cffa1a0721384
Glen Popowich <glenp@telus.net>
2025-11-04 20:34:55 -0800

    added context and language icons

--- src/components/NavigationMenu/NavigationMenu.tsx
+++ src/components/NavigationMenu/NavigationMenu.tsx
@@ -1,8 +1,25 @@
-import React from "react";
-import { NavigationMenuContainer } from "./NavigationMenu.styles";
+import React, { useContext } from "react";
+import {
+  NavigationMenuAction,
+  NavigationMenuContainer,
+} from "./NavigationMenu.styles";
+import { AppContext } from "../AppWrapper/AppWrapper";
+import { RxFontFamily } from "react-icons/rx";
+import { RiEnglishInput } from "react-icons/ri";
 
 const NavigationMenu = () => {
-  return <NavigationMenuContainer>NavigationMenu</NavigationMenuContainer>;
+  const { currentLanguage, theme } = useContext(AppContext);
+  const languageIcon =
+    currentLanguage === "en" ? (
+      <RxFontFamily color={theme.textColor} size={"1.2vw"} />
+    ) : (
+      <RiEnglishInput color={theme.textColor} size={"1.2vw"} />
+    );
+  return (
+    <NavigationMenuContainer>
+      <NavigationMenuAction>{languageIcon}</NavigationMenuAction>
+    </NavigationMenuContainer>
+  );
 };
 
 export default NavigationMenu;
